c62dda6b9ebf8a070d29c256de57412060b6d9dc
Sylvester Menawar <sylvesternathan93@gmail.com>
2020-12-15 14:34:39 +0000

    added header component aand installed material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-     <h1>Facebook app</h1>
+     <Header />
+     {/* app body */}
+     {/* sidebar */}
+     {/* Feed */}
+     {/* widgets */}
     </div>
   );
 }
